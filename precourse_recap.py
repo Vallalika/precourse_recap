f002988cacd526cd198435477f00fb6b43a8e60d
Valerie Bayard <valerie.bayard@gmail.com>
2022-03-28 16:29:29 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,20 @@
+print("Sentence:")
+name = "Valerie"
+years = 5
+country = "the United Kingdom"
+print(name + " has lived " + str(years) + " years in " + country)
+
+
+print(" String multiplication:")
+animal = "cat "
+multiplier = 10
+new_sentence = animal * multiplier
+
+print(new_sentence.upper())
+
+
+print("Calculations:")
+number_1 = 10
+number_2 = 118
+calculation = (number_1 + number_2) / 2
+print(calculation)
